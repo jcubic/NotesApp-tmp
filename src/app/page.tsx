8627f48bab59dd40e045226bbd5a017a934ab216
Jakub Jankiewicz <jcubic@onet.pl>
2024-08-13 22:15:58 +0200

    main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,25 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Image src="/logo.svg" alt="SNApp logo" fill/>
-    </main>
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <div className={styles.logo}>
+            <Image src="/logo.svg" alt="SNApp logo" fill/>
+          </div>
+          <span>SNApp</span>
+        </header>
+        <aside className={styles.notes}>
+
+        </aside>
+        <div className={styles.editor}>
+          <textarea></textarea>
+        </div>
+        <aside className={styles.filter}>
+
+        </aside>
+        <footer className={styles.footer}>
+          <p>Copyright (c) 2024 Jakub T. Jankiewicz</p>
+        </footer>
+      </main>
   );
 }

de2ed35a997b22f0c207d4c526459e35b0e239df
Jakub Jankiewicz <jcubic@onet.pl>
2024-08-15 21:06:42 +0200

    fix TreeView and action icon

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { MouseEvent } from 'react';
 import { IconChevronDown } from '@tabler/icons-react';
 import { Group, Tree, Flex, TreeNodeData, NavLink } from '@mantine/core';
@@ -7,7 +6,6 @@ import { IconFileText, IconFilePlus } from '@tabler/icons-react';
 import type { RenderTreeNodePayload } from '@mantine/core';
 
 import ToolbarIcon from './ToolbarIcon';
-
 
 type Node = RenderTreeNodePayload['node'];
 type AnchorMouseEvent = MouseEvent<HTMLAnchorElement>;
@@ -32,39 +30,53 @@ export default function TreeView({ data }: TreeProps) {
       data={data}
       levelOffset={23}
       expandOnClick={false}
-      renderNode={({ node, tree, expanded, hasChildren, elementProps, selected }: RenderTreeNodePayload) => (
-        <Group gap={5} {...elementProps}>
-          {hasChildren && (
-            <Flex
-              justify="flex-start"
-              align="center"
-              direction="row"
-            >
-              <IconChevronDown
-                size={18}
-                style={{ transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
-              />
+      renderNode={({ node, tree, expanded, hasChildren, elementProps, selected }: RenderTreeNodePayload) => {
+        function toggle(event: AnchorMouseEvent) {
+          ignoreEvent(event);
+          tree.toggleExpanded(node.value);
+        }
+        return (
+          <Group gap={5} {...elementProps}>
+            {hasChildren && (
+              <Flex
+                justify="flex-start"
+                align="center"
+                direction="row"
+              >
+                <span onClick={toggle}>
+                  <IconChevronDown
+                    size={18}
+                    style={{ transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
+                  />
+                </span>
+                <Flex
+                  direction="row"
+                  gap={5}>
+                  <NavLink
+                    href="#required-for-focus"
+                    active={selected}
+                    h={28}
+                    onClick={toggle}
+                    label={node.label}
+                  />
+                  <ToolbarIcon icon={<IconFilePlus size={14}/>} onClick={() => {
+                    newNote();
+                    console.log(tree.hoveredNode);
+                  }}/>
+                </Flex>
+              </Flex>
+            )}
+            {!hasChildren && (
               <NavLink
                 href="#required-for-focus"
-                active={selected}
-                onClick={(event) => { ignoreEvent(event); tree.toggleExpanded(node.value); }}
-                label={node.label}/>
-              <ToolbarIcon icon={<IconFilePlus size={14}/>} onClick={() => {
-                newNote();
-                console.log(tree.hoveredNode);
-              }}/>
-            </Flex>
-          )}
-          {!hasChildren && (
-            <NavLink
-              href="#required-for-focus"
-              label={node.label}
-              onClick={(event) => { ignoreEvent(event); tree.select(node.value); }}
-              leftSection={<IconFileText size="1rem" stroke={1.5} />}
-            />
-          )}
-        </Group>
-      )}
+                label={node.label}
+                onClick={(event) => { ignoreEvent(event); tree.select(node.value); }}
+                leftSection={<IconFileText size="1rem" stroke={1.5} />}
+              />
+            )}
+          </Group>
+        );
+      }}
     />
   );
 }

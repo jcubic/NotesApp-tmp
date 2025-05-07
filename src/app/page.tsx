1ba7dc9ebb17c807d5939c361e464fc842c32a05
Jakub Jankiewicz <jcubic@onet.pl>
2025-05-07 21:15:53 +0200

    2 space indent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,24 @@ import NotesPanel from '@/components/NotesPanel';
 
 export default function Home() {
   return (
-      <main className={styles.main}>
-        <header className={styles.header}>
-          <div className={styles.logo}>
-            <Image src="/logo.svg" alt="SNApp logo" fill/>
-          </div>
-          <span>SNApp</span>
-        </header>
-        <aside className={styles.notes}>
-          <NotesPanel />
-        </aside>
-        <div className={styles.editor}>
-          <textarea></textarea>
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Image src="/logo.svg" alt="SNApp logo" fill/>
         </div>
-        <aside className={styles.filter}>
-        </aside>
-        <footer className={styles.footer}>
-          <p>Copyright (c) 2024 Jakub T. Jankiewicz</p>
-        </footer>
-      </main>
+        <span>SNApp</span>
+      </header>
+      <aside className={styles.notes}>
+        <NotesPanel />
+      </aside>
+      <div className={styles.editor}>
+        <textarea></textarea>
+      </div>
+      <aside className={styles.filter}>
+      </aside>
+      <footer className={styles.footer}>
+        <p>Copyright (c) 2024 Jakub T. Jankiewicz</p>
+      </footer>
+    </main>
   );
 }
